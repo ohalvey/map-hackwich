6f4ecaaad86388525c5667a4364bba375dd52e6d
oliver halvey <oliverhalvey310@yahoo.com>
2023-02-08 07:40:33 -0800

    Initial Commit

---
+++ map hackwich/map_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_hackwichApp.swift
+//  map hackwich
+//
+//  Created by Oliver Halvey on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct map_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
